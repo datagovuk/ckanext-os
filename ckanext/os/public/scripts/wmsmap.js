c1379db6a8079959773e60d85c5eae8c8b93d9ec
David Read <david.read@hackneyworkshop.com>
2012-12-20 16:26:31 +0000

    Removing bracket to get gazeteer working on Chrome.

--- ckanext/os/public/scripts/wmsmap.js
+++ ckanext/os/public/scripts/wmsmap.js
@@ -613,7 +613,7 @@ function gazetteer(queryText) {
         if (navigator.appName == "Microsoft Internet Explorer"){
             xmlhttp.onreadystatechange = new Function(handleGazServerResponse());
 	} else {
-            xmlhttp.onreadystatechange = handleGazServerResponse();
+            xmlhttp.onreadystatechange = handleGazServerResponse;
         }
         xmlhttp.send(null);
     }

799d7cfa7c6bede64dec1e83bc6d94afbd0b3151
David Read <david.read@hackneyworkshop.com>
2013-01-08 12:07:55 +0000

    Add spinner.

--- ckanext/os/public/scripts/wmsmap.js
+++ ckanext/os/public/scripts/wmsmap.js
@@ -438,8 +438,8 @@ function activateKeyboardDefault() {
 
 // Process the Search query
 function processQuery() {
-		showBusysign();
-		cursor_wait();
+    SearchSpinner.start()
+    cursor_wait();
 		
     // Hide and clear list box
     var thedropdown = document.getElementById('selectGaz');
@@ -513,7 +513,7 @@ function processQuery() {
         gazetteer(queryText);
     }
     
-    hideBusysign();
+    SearchSpinner.stop()
     setTimeout("cursor_clear()", 50);
 }
 
@@ -1454,9 +1454,41 @@ function fixcase(str) {
     });
 }
 
-// Bootstrap tooltip
-$(function() {
+$(
+  // Bootstrap tooltip
+  function() {
   $('.htmltooltip').popover({
     placement: 'right'
       })
-    });
+
+  // Singleton object to handle spinner animation. Call start() and stop()
+  SearchSpinner = {
+    el: $('.search-spinner')[0],
+    config: {
+      lines: 9, // The number of lines to draw
+      length: 4, // The length of each line
+      width: 2, // The line thickness
+      radius: 3, // The radius of the inner circle
+      rotate: 0, // The rotation offset
+      color: '#000', // #rgb or #rrggbb
+      speed: 2, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    },
+    active: null,
+    start: function() {
+      if (this.active) return;
+      this.active = new Spinner(this.config).spin(this.el);
+    },
+    stop: function() {
+      if (!this.active) return;
+      this.active.stop();
+      this.active = null;
+    }
+  };
+})
